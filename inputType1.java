911d180c4a03637ce0f6a232d686ac93e875dcd4
alekhyamahathi1 <alekhyamahathi1@gmail.com>
2025-07-06 23:50:05 +0530

    new files

---
+++ inputType1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class inputType1 {
+public static void main (String args[] ) {
+Scanner inputObject = new Scanner(System.in);
+System.out.println("Enter your Input Value");
+String ObjName = inputObject.nextLine();
+System.out.println("Your input value is " + ObjName ); } }
