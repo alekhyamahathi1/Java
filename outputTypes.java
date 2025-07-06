58f260e0ab4394d52991d1ef8808fae16ef6de5d
alekhyamahathi1 <alekhyamahathi1@gmail.com>
2025-07-07 00:26:54 +0530

    hello

---
+++ outputTypes.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class outputTypes {
+public static void main (String args[]) {
+System.out.print("Hello World, ");
+System.out.println("Hello world");
+System.out.printf("heyyyy"); } }
