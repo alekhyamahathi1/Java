1e497fa4870ddb37b990cc22a60e5ac0c794fe45
alekhyamahathi1 <alekhyamahathi1@gmail.com>
2025-07-09 16:58:26 +0530

    new

---
+++ arrayInput.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+
+class arrayInput {
+    public void sortColors(int[] nums) {
+        Scanner myObj = new Scanner(System.in);
+        int n[] = myObj.nextInt();
+
+        int[] nums = new int[n]; 
+
+         for (int i = 0; i < n; i++) {
+            nums[i] = myObj.nextInt(); 
+            }
+            System.out.println(Arrays.nums());
+            myObj.close();
+        
+    } 
+}  
