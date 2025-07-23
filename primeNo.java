79c46cd9065e7e4d8cec5484fd6280d265697ea7
alekhyamahathi1 <alekhyamahathi1@gmail.com>
2025-07-23 21:06:27 +0530

    new

--- primeNo.java
+++ primeNo.java
@@ -1,24 +1,45 @@
-import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class primeNo {
-public static void main (String[] args) {
-Scanner myObj = new Scanner(System.in);
-System.out.println("Enter a number");
-int Number = myObj.nextInt();
 
-if (Number > 1){
+    public static boolean isPrime(int number) {
 
-for(int n = 2 ; n < Number ; n++) {
-if (Number % n == 0) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number == 2) {
+            return true;
+        }
+        if (number == 3) {
+            return true;
+        }
 
-System.out.println("Not Prime");
-continue;
-} else { 
-System.out.println("Prime");
-}}
+        if (number % 2 == 0) {
+            return false;
+        }
 
+        for (int i = 3; i * i <= number; i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+
+        int inputNumber = scanner.nextInt();
+
+        if (isPrime(inputNumber)) {
+            System.out.println(inputNumber + " is a prime number.");
+        } else {
+            System.out.println(inputNumber + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
 }
-
-else {
-System.out.println("Enter Whole Number");} }}
